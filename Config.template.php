a2089d04ad5b90157cd56c591be71c57c17f1044
magkopian <m.agkopian@gmail.com>
2014-10-30 23:08:12 +0200

    Added Config class

---
+++ Config.template.php
@@ -0,0 +1,12 @@
+<?php
+/**********************************************\
+* Copyright (c) 2014 Manolis Agkopian          *
+* See the file LICENCE for copying permission. *
+\**********************************************/
+
+class Config {
+	const DB_NAME = '';
+	const DB_HOST = '127.0.0.1';
+	const DB_USER = '';
+	const DB_PASSWORD = '';
+}
--- src/classes/Database.php
+++ src/classes/Database.php
@@ -5,13 +5,9 @@
 \**********************************************/
 
 class Database extends PDO {
-	const DATABASE = 'inf_comments';
-	const HOST = '127.0.0.1';
-	const USER = 'Manolis';
-	const PASSWORD = 'qwe/789';
 
 	public function __construct() {
-		parent::__construct('mysql:host=' . $this::HOST . ';dbname=' . $this::DATABASE, $this::USER, $this::PASSWORD);
+		parent::__construct('mysql:host=' . Config::DB_HOST . ';dbname=' . Config::DB_NAME, Config::DB_USER, Config::DB_PASSWORD);
 		$this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 		$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	}
